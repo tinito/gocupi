967489745ded7dbabaef8ddca11504bf1a492ccb
Martino Migliavacca <martino.migliavacca@gmail.com>
2017-01-03 10:53:39 +0100

    Updated plotinum repository

--- polargraph/chart.go
+++ polargraph/chart.go
@@ -1,7 +1,7 @@
 package polargraph
 
 import (
-	chart "github.com/p/plotinum/plot"
+	chart "github.com/gonum/plot"
     chartplotter "github.com/gonum/plot/plotter"
 	vg "github.com/gonum/plot/vg"
 	"image/color"
